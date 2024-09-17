cd2c0be0736037f8e411f02d6c52598468b7d08a
krutika thakkar <thakkarkrutika4@gmail.com>
2024-09-17 10:19:03 +0530

    Finetuned Explicit Animations

--- mealapp/lib/screens/categoriesItems.dart
+++ mealapp/lib/screens/categoriesItems.dart
@@ -75,11 +75,17 @@ class _CategoriesitemsState extends State<Categoriesitems> with SingleTickerProv
             )
         ],
       ),
-      builder: (context, child) => Padding(
-        padding: EdgeInsets.only(
-          top: 100 - _animationController.value * 100,
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
         ),
-        child: child,
+        child: child ,
       ),
     );
   }
